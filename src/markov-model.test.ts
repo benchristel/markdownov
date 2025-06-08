f1a6aed0b8f71192c8eadb41c13c4291a53c7e94
Ben Christel <ben.christel@gmail.com>
2025-06-07 20:18:06 -0700

    Implement order-1 Markov model

--- src/markov-model.test.ts
+++ src/markov-model.test.ts
@@ -1,9 +1,16 @@
 import {test, expect, is} from "@benchristel/taste"
 import {MarkovModel} from "./markov-model.js"
+import {seedRandom} from "./random.js"
 
 test("a MarkovModel", {
     "generates nothing when not trained"() {
-        const model = new MarkovModel()
+        const model = new MarkovModel(seedRandom(""))
         expect(model.generate(), is, "")
+    },
+
+    "generates a sequence of tokens based on training data"() {
+        const model = new MarkovModel(seedRandom(""))
+        model.train("a a a a a")
+        expect(model.generate(), is, "a a a a a a a a a")
     },
 })
--- src/markov-model.ts
+++ src/markov-model.ts
@@ -1,5 +1,39 @@
+import {tokenize} from "./tokenize.js"
+
+const EOT = ""
+
 export class MarkovModel {
-    generate(): string {
-        return ""
+    private transitions: Record<string, string[]> = {
+        [EOT]: [],
     }
+
+    constructor(private rng: () => number) {}
+
+    train(text: string) {
+        // TODO: hardcoded order
+        const tokens = [EOT, ...tokenize(text), EOT]
+        // TODO: hardcoded order
+        for (let i = 1; i < tokens.length; i++) {
+            const token = tokens[i]
+            const previous = tokens[i - 1]
+            this.transitions[previous] ??= []
+            this.transitions[previous].push(token)
+        }
+    }
+
+    generate(): string {
+        let generated = [EOT]
+        // TODO: magic number
+        for (let i = 0; i < 42; i++) {
+            const last = generated.at(-1) ?? EOT
+            const next = pickRandom(this.rng, this.transitions[last], EOT)
+            generated.push(next)
+            if (next === EOT) break
+        }
+        return generated.join("")
+    }
+}
+
+function pickRandom<T>(rng: () => number, array: T[], defaultValue: T): T {
+    return array[Math.floor(rng() * array.length)]
 }
