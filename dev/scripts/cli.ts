7021ff998d81ecbe8070d80076fc32ddd4bdb095
Ben Christel <ben.christel@gmail.com>
2025-06-08 09:04:11 -0700

    Remove console.log

--- dev/scripts/cli.ts
+++ dev/scripts/cli.ts
@@ -5,8 +5,6 @@ import {Order2} from "../../src/markov/order2"
 
 const paths = process.argv.slice(2)
 
-console.log(paths)
-
 const model = new MarkovModel(Math.random, new Order2())
 
 async function trainOn(path: string): Promise<void> {
